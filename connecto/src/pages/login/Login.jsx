7cd260f406a5bd64a75ef6258bcb88b8f6c759c8
Inshira Karim <karim.inshira@gmail.com>
2023-11-08 00:36:45 +0530

    checkpoint-1 Designed the login page

--- connecto/src/pages/login/Login.jsx
+++ connecto/src/pages/login/Login.jsx
@@ -4,7 +4,22 @@ import './login.css'
 function Login() {
   return (
     <div className='login'>
-    Hellllooo!
+    <div className='card'>
+        <div className='left'>
+            <h1>Hey there!</h1>
+             <p>Welcome to Connecto - a safe place for all your connections.</p>
+             <span>Don't have an account?</span>
+             <button>Register</button>     
+        </div>
+        <div className='right'>
+        <h1>Login</h1>
+            <form>
+              <input type="text" placeholder="Username"/>
+                <input type="password" placeholder="Password"/>
+                <button>Login</button>
+            </form>
+        </div>
+    </div>
     </div>
   )
 }
