f22bfa098b4fb294fe7f22ee89eb509a8b485528
Inshira Karim <karim.inshira@gmail.com>
2023-11-12 00:49:10 +0530

    Designed story viewing component

---
+++ connecto/src/components/Stories/Stories.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './stories.css'
+function Stories() {
+  return (
+    <div className='stories'>
+        <div className='story'>
+        <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Inshira</span>
+              <button>+</button>
+        </div>
+        <div className='story'>
+        <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Inshira</span>
+        </div>
+        <div className='story'>
+        <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Inshira</span>
+        </div>
+        <div className='story'>
+        <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Inshira</span>
+        </div>
+        <div className='story'>
+        <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Inshira</span>
+        </div>
+    </div>
+  )
+}
+
+export default Stories
